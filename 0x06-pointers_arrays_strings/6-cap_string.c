d2355b46a8a55e47402ae167e5b1bf5f9bff7907
THaigz <leongithaiga@gmail.com>
2022-06-16 17:24:30 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalization function
+ * @x: string parameter
+ * Return: capitalized string
+ */
+char *cap_string(char *x)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int len = 13;
+	int a = 0, i;
+
+	while (x[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || x[a - a] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				x[a] = x[a] - 32;
+			i++;
+		}
+		a++;
+	}
+	return (x);
+}
